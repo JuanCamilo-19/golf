57fb7d5ec7326fad8270caf801c50db9ced09271
juancamilo-19 <velajuancamilo1@gmail.com>
2024-10-22 17:22:06 -0500

    cracion del modelo y migracion de reservas

--- app/Models/CampoGolf.php
+++ app/Models/CampoGolf.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class CampoGolf extends Model
 {
-    //
+     protected $fillable = ['nombre', 'ubicacion', 'numero_hoyos'];
+
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class);
+    }
 }
--- database/migrations/2024_10_22_004007_create_jugadors_table.php
+++ database/migrations/2024_10_22_004007_create_jugadors_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('campo_golves', function (Blueprint $table) {
+        Schema::create('jugadors', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('ubicacion');
-            $table->integer('numero_hoyos');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('campo_golves');
+        Schema::dropIfExists('jugadors');
     }
 };
